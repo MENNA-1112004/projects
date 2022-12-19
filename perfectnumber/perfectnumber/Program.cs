a03c2497b63c5be68acbdaab13fdaa9fb6fbb874
menna <116295897+MENNA-1112004@users.noreply.github.com>
2022-12-20 00:13:46 +0200

    Add files via upload

---
+++ perfectnumber/perfectnumber/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter the first num");
+int first_num = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second num");
+int second_num = int.Parse(Console.ReadLine());
+
+for (int i = first_num; i <= second_num; i++)
+{
+    int sum = 0;
+    for (int j = 1; j < i; j++)
+    {
+        if (i % j == 0)
+        {
+            sum += j;
+        }
+
+
+    }
+
+    if (sum == i && i != 0)
+        Console.WriteLine(i);
+
+
+
+}
